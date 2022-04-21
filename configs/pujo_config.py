a6767f84bffc06b1e2dde10cf859d84eb8d6809b
Deanazor <arief.p.a@mail.ugm.ac.id>
2022-04-21 16:06:28 +0700

    update configs

--- configs/pujo_config.py
+++ configs/pujo_config.py
@@ -7,7 +7,7 @@ from easydict import EasyDict as edict
 config = edict()
 config.margin_list = (1.0, 0.0, 0.4)
 config.network = "r100"
-config.resume = True
+config.resume = False
 config.output = "SavedModels"
 config.embedding_size = 512
 config.sample_rate = 1.0
@@ -16,12 +16,12 @@ config.momentum = 0.9
 config.weight_decay = 5e-4
 config.batch_size = 256
 config.lr = 0.4
-config.verbose = 50
+config.verbose = 500
 config.dali = False
 
-config.rec = "train_tmp/glint360k"
-config.num_classes = 360232
-config.num_image = 17091657
+config.rec = "train_tmp/faces_emore"
+config.num_classes = 85000
+config.num_image = 5800000
 config.num_epoch = 20
 config.warmup_epoch = 2
 config.val_targets = ['lfw']
